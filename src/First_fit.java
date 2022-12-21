4627698b298f154c63ec2a68c0ad990d25030227
Mohamed Amir <m.elamir027@gmail.com>
2022-12-21 15:40:23 +0200

    firstFit finished

--- src/First_fit.java
+++ src/First_fit.java
@@ -1,51 +1,47 @@
 package src;
+
 import java.util.Formatter;
 import java.util.Vector;
 
 public class First_fit {
-    public First_fit(){
+    public First_fit() {
     }
 
-    void first_fit(Vector<partition> partitions, Vector<process> processes){
-
+    void first_fit(Vector<partition> partitions, Vector<process> processes) {
         for (int i = 0; i < processes.size(); i++) {
             for (int j = 0; j < partitions.size(); j++) {
                 if (partitions.get(j).getPartitionSize() >= processes.get(i).getProcessSize() && !processes.get(i).getTaken()) {
                     processes.get(i).setTaken(true);
-                    if(partitions.get(j).getPartitionSize()>processes.get(i).getProcessSize()){
+                    if (partitions.get(j).getPartitionSize() > processes.get(i).getProcessSize()) {
+                        int sz = partitions.get(j).getPartitionSize() - processes.get(i).getProcessSize();
                         partitions.get(j).setPartitionSize(processes.get(i).getProcessSize());
                         partitions.get(j).setReferToProcess(processes.get(i).getProcessName());
-                        partitions.add(new partition("Part"+partitions.size(),partitions.get(j).getPartitionSize()-processes.get(i).getProcessSize()));
-                    }
-                    else if(partitions.get(j).getPartitionSize()==processes.get(i).getProcessSize()){
+                        partitions.insertElementAt(new partition("Part" + partitions.size(), sz), j + 1);
+                        break;
+                    } else if (partitions.get(j).getPartitionSize() == processes.get(i).getProcessSize()) {
                         partitions.get(j).setPartitionSize(processes.get(i).getProcessSize());
                         partitions.get(j).setReferToProcess(processes.get(i).getProcessName());
+                        break;
                     }
                 }
             }
         }
 
         Formatter fmt = new Formatter();
-        fmt.format("%15s %15s %15s", "partitionName", "Partition_size","processName");
+        fmt.format("%13s %15s %15s", "partitionName", "Partition_size", "processName");
         System.out.println(fmt);
         for (int i = 0; i < partitions.size(); i++) {
             Formatter fmt1 = new Formatter();
-            fmt1.format("%15s %15s ", partitions.get(i).getPartitionName(), partitions.get(i).getPartitionSize());
+            if (partitions.get(i).getReferToProcess() != null) {
+                fmt1.format("%12s %10s %13s", partitions.get(i).getPartitionName(), partitions.get(i).getPartitionSize(), partitions.get(i).getReferToProcess());
+            } else {
+                fmt1.format("%12s %10s %27s", partitions.get(i).getPartitionName(), partitions.get(i).getPartitionSize(), "External fragment");
+            }
             System.out.println(fmt1);
-            if(partitions.get(i).getReferToProcess()!=null){
-                Formatter fmt2 = new Formatter();
-                fmt2.format(" %15s ", partitions.get(i).getReferToProcess());
-                System.out.println(fmt2);
-            }
-            else {
-                Formatter fmt3 = new Formatter();
-                fmt3.format(" %15s ", "External fragment");
-                System.out.println(fmt3);
-            }
         }
-        for(int i=0; i<processes.size(); i++){
-            if(!processes.get(i).getTaken()){
-                System.out.println(processes.get(i).getProcessName()+" can not be allocated\n");
+        for (int i = 0; i < processes.size(); i++) {
+            if (!processes.get(i).getTaken()) {
+                System.out.println(processes.get(i).getProcessName() + " can not be allocated\n");
             }
         }
     }
